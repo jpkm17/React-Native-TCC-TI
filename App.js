ae36c25a24692992c857bb8f59ef04be66597229
João P. Meneguesso <jpm83633@gmail.com>
2022-05-11 09:11:28 -0300

    Tarefa estilizando tela de login

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, Image, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import { styles } from './styles/style';
+import ganes from './assets/logo.png';
+
 
 function HomeScreen({ navigation }) {
   return (
@@ -35,16 +39,14 @@ const Drawer = createDrawerNavigator();
 
 function MainDrawer() {
   return (
-
     <Drawer.Navigator initialRouteName="Home">
       <Drawer.Screen name="Home" component={HomeScreen} />
       <Drawer.Screen name="Notifications" component={NotificationsScreen} />
       <Drawer.Screen name="Settings" component={Settings} />
-      <Drawer.Screen name="Cadastro" component={Cadastro}/>
+      <Drawer.Screen name="Cadastro" component={Cadastro} />
     </Drawer.Navigator>
   );
 }
-
 
 function Profile() {
   return (
@@ -65,17 +67,33 @@ function Settings({ navigation }) {
 
 function Login({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Login Screen</Text>
-      <Button
-        title="Go to MainDrawer"
-        onPress={() => navigation.navigate('MainDrawer')}
+    <View style={styles.container}>
+      <Image source={ganes} style={styles.logo} />
+      <Text style={styles.login}>LOGIN</Text>
+
+      <TextInput
+        style={styles.input1}
+        placeholder="Digite seu Email"
+      />
+
+      <TextInput
+        style={styles.input2}
+        placeholder="Digite sua Senha"
       />
 
       <Button
+        color="#a80601"
+        onPress={() => navigation.navigate('MainDrawer')}
+        title="Logar"
+        style={styles.button}
+      />
+      <Text>{'\n'}</Text>
+      
+      <Button
         title="Cadastro"
         onPress={() => navigation.navigate('Cadastro')}
-      />
+        style={styles.button}
+      /> 
     </View>
   );
 }
@@ -84,7 +102,7 @@ function Cadastro({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Tela de cadastro</Text>
-      
+
       <Button
         title="Go to MainDrawer"
         onPress={() => navigation.navigate('MainDrawer')}
@@ -107,7 +125,7 @@ function App() {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Profile" component={Profile} />
         <Stack.Screen name="Settings" component={Settings} />
-        <Stack.Screen name="Cadastro" component={Cadastro}/>
+        <Stack.Screen name="Cadastro" component={Cadastro} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
